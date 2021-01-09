f679632fe6bc3f5bf327bfd7a5962009aad436f4
IvanVarabei <garlic9991@gmail.com>
2021-01-09 09:49:30 +0300

    add constraint on delete cascade

--- core/src/main/resources/db.migration/V2__insert_data.sql
+++ core/src/main/resources/db.migration/V2__insert_data.sql
@@ -1,13 +1,13 @@
---todo create 2 certificates
---todo create 2 tags
---todo create relations
-INSERT INTO public.gift_certificate(id, name, description, price, duration, create_date, last_update_date)
-VALUES ('1', 'certificate 1', 'description 1', 100, 5, now()::timestamp, now()::timestamp);
+INSERT INTO public.gift_certificate(id, name, description, price, duration)
+VALUES (1, 'goldie''s gym', '5 free visits', 9.99, 7),
+       (2, 'Kfc birthday', '50% off', 5.55, 16);
 
 
 INSERT INTO public.tag(id, name)
-VALUES (1, 'tag 1');
+VALUES (1, 'gym'),
+       (2, 'cheap');
 
 
 INSERT INTO public.certificate_tag(gift_certificate_id, tag_id)
-VALUES (1, 1);
+VALUES (1, 1),
+       (1, 2);
