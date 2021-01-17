f4af19916165e8007fd2919ede09b0f2cd03a2d4
IvanVarabei <garlic9991@gmail.com>
2021-01-17 12:45:31 +0300

    remove jdbcTemplate from TagRepositoryImplTest

--- api/src/test/java/com/epam/esm/repository/TagRepositoryImplTest.java
+++ api/src/test/java/com/epam/esm/repository/TagRepositoryImplTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -24,8 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @WebAppConfiguration
 @ContextConfiguration(classes = {EmbeddedTestConfig.class})
 class TagRepositoryImplTest {
-    @Autowired
-    JdbcTemplate jdbcTemplate;
     @Autowired
     TagRepository tagRepository;
     @Autowired
