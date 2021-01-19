ca544ed074239b942d4d2c7e6b23a3247297de90
IvanVarabei <garlic9991@gmail.com>
2021-01-19 11:45:18 +0300

    fix certificate mapper

--- core/src/main/java/com/epam/esm/mapper/CertificateMapper.java
+++ core/src/main/java/com/epam/esm/mapper/CertificateMapper.java
@@ -1,20 +1,16 @@
 package com.epam.esm.mapper;
 
 import com.epam.esm.entity.GiftCertificate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
 @Component
-@RequiredArgsConstructor
 public class CertificateMapper implements RowMapper<GiftCertificate> {
     @Override
     public GiftCertificate mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -24,18 +20,10 @@ public class CertificateMapper implements RowMapper<GiftCertificate> {
         giftCertificate.setDescription(rs.getString("description"));
         giftCertificate.setPrice(rs.getBigDecimal("price"));
         giftCertificate.setDuration(rs.getInt("duration"));
-        Instant instant = rs.getTimestamp("create_date").toInstant();
-        ZonedDateTime zd = ZonedDateTime.ofInstant(instant, ZoneId.of("UTC"));
-        LocalDateTime ld = zd.toLocalDateTime();
-        giftCertificate.setCreatedDate(ZonedDateTime
-                .ofInstant(rs.getTimestamp("create_date").toInstant(), ZoneId.of("UTC"))
-                .toLocalDateTime());
-        giftCertificate.setUpdatedDate(ZonedDateTime
-                .ofInstant(rs.getTimestamp("last_update_date").toInstant(), ZoneId.of("UTC"))
-                .toLocalDateTime());
-
-        //        giftCertificate.setCreatedDate(rs.getTimestamp("create_date").toLocalDateTime());
-//        giftCertificate.setUpdatedDate(rs.getTimestamp("last_update_date").toLocalDateTime());
+        giftCertificate.setCreatedDate(ZonedDateTime.ofInstant(
+                rs.getTimestamp("create_date").toInstant(), ZoneOffset.UTC).toLocalDateTime());
+        giftCertificate.setUpdatedDate(ZonedDateTime.ofInstant(
+                rs.getTimestamp("last_update_date").toInstant(), ZoneOffset.UTC).toLocalDateTime());
         giftCertificate.setTags(new ArrayList<>());
         return giftCertificate;
     }
